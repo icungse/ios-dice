88ee663f88f779a150321b9801f8eb104a5a8671
icungse <icungse@gmail.com.com>
2019-02-21 19:18:26 +0700

    Work with random number for dice

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var rollButton: UIButton!
@@ -22,6 +26,11 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        randomDiceIndex1 = Int.random(in: 0..<7)
+        randomDiceIndex2 = Int.random(in: 0..<7)
+        
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
         
     }
     
