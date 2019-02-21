6fc0ff50d13cc882a93d7fde08d28b0227f1b756
icungse <icungse@gmail.com.com>
2019-02-21 21:14:01 +0700

    Finish building app with worked roll dice and init README.MD

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var rollButton: UIButton!
@@ -29,8 +31,8 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int.random(in: 0..<7)
         randomDiceIndex2 = Int.random(in: 0..<7)
         
-        diceImageView1.image = UIImage(named: "dice2")
-        diceImageView2.image = UIImage(named: "dice2")
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
     
